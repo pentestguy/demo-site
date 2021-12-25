6805900dab70a2ca3f4a59e1913b1e1de8bc0e82
Shubham Nagdive <nagdiveshubham850@gmail.com>
2021-12-26 00:49:36 +0530

    add: initialCommit

---
+++ src/components/body/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Intro from './about/index'
+
+function Body() {
+    return (
+        <div>
+            <Intro />
+        </div>
+    )
+}
+
+export default Body
---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+    return (
+        <div>
+            This is footer;
+        </div>
+    )
+}
+
+export default index
